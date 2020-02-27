f7fbfe03c9607bf450fcdc33b894fac76ffdcfa2
marcusreese <marcus.reese@caci.com>
2020-02-27 16:50:11 +0000

    fix #3; rename if duplicate filename

--- public/main-file-change.js
+++ public/main-file-change.js
@@ -1,13 +1,17 @@
 
+const getName = file => {
+    return app.fileResults[file.name] ?
+        file.name + ' ' + new Date(file.lastModified).toLocaleString() : file.name;
+};
 document.querySelector('#file-input').addEventListener('change',  event => {
     const fileArray = Array.from(event.target.files);
-    app.showMoreInList(fileArray.map(file => file.name), 'files');
+    app.showMoreInList(fileArray.map(file => getName(file)), 'files');
     app.ready = false;
     let numFiles = 0;
     fileArray.forEach(file => {
         // Note: app.files will not necessarily be in the same order as the displayed list
         file.text().then(text => {
-            app.files.push({name: file.name, lines: text.split('\n')});
+            app.files.push({name: getName(file), lines: text.split('\n')});
             if (++numFiles === fileArray.length) {
                 app.ready = true;
                 app.doSearch();
